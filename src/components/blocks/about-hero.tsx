8bee633ac56e55fb351b0ccc63f3097f79bad91b
Ader Oliver <63608525+daoliveirafilho@users.noreply.github.com>
2026-06-13 09:24:54 -0300

    Add files via upload

--- src/components/blocks/about-hero.tsx
+++ src/components/blocks/about-hero.tsx
@@ -23,31 +23,22 @@ export function AboutHero() {
   return (
     <section className="">
       <div className="container flex max-w-5xl flex-col justify-between gap-8 md:gap-20 lg:flex-row lg:items-center lg:gap-24 xl:gap-24">
-        <div className="flex-[1.5]">
-          <h1 className="text-3xl tracking-tight sm:text-4xl md:text-5xl lg:text-6xl">
-            Democratising quality software
-          </h1>
+<div className="flex-[1.5]">
+<h1 className="text-3xl tracking-tight sm:text-4xl md:text-5xl lg:text-6xl">
+Rekall
+</h1>
 
-          <p className="text-muted-foreground mt-5 text-2xl md:text-3xl lg:text-4xl">
-            Mainline is bringing modern software to life with AI magic.
-          </p>
+<p className="text-muted-foreground mt-5 text-2xl md:text-3xl lg:text-4xl">
+Log all data.
+</p>
 
-          <p className="text-muted-foreground mt-8 hidden max-w-lg space-y-6 text-lg text-balance md:block lg:mt-12">
-            At Mainline, we are dedicated to transforming the way teams plan,
-            execute, and deliver projects. Our mission is to provide our
-            customers with an unbeatable edge over delays, inefficiencies, and
-            disorganisation through actionable insights and seamless
-            collaboration. We’ll stop at nothing to give you the tools you need
-            to get every project across the finish line.
-            <br />
-            <br />
-            We’re customer-obsessed — investing the time to understand every
-            aspect of your workflow so that we can help you operate better than
-            ever before. We’re all in this together because your success is our
-            success. In our history as a company, we’ve never lost a customer,
-            because when your projects succeed, so do we.
-          </p>
-        </div>
+<p className="text-muted-foreground mt-8 hidden max-w-lg space-y-6 text-lg text-balance md:block lg:mt-12">
+Construir solu&ccedil;&otilde;es por meio de computa&ccedil;&atilde;o confi&aacute;vel, da nuvem &agrave; borda. Consolidar-se no mercado de solu&ccedil;&otilde;es computacionais, em comprometimento com inova&ccedil;&atilde;o e adequa&ccedil;&atilde;o. Qualidade e confiabilidade de servi&ccedil;o. Transpar&ecirc;ncia e verdade na atua&ccedil;&atilde;o e inclus&atilde;o digital.
+<br />
+<br />
+Eliminando a complexidade dos gateways tradicionais e entregando seus dados diretamente em uma infraestrutura distribu&iacute;da, assegurando performance e escalabilidade.
+</p>
+</div>
 
         <div
           className={`relative flex flex-1 flex-col justify-center gap-3 pt-10 lg:pt-0 lg:pl-10`}
